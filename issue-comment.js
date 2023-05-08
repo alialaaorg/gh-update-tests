308610e8c91afdde1a31f9847cc8e6ec6f88619c
alialaa <hello@alialaa.com>
2023-05-08 14:11:36 +0300

    daad

--- issue-comment.js
+++ issue-comment.js
@@ -1,7 +1,7 @@
-const { getRandomJoke } = require("random-jokes");
+const { getDadjoke } = require("random-jokes");
 
 module.exports = async ({ github, context }) => {
-  const joke = await getRandomJoke();
+  const joke = await getDadjoke();
   console.log(joke);
   github.rest.issues.createComment({
     issue_number: context.issue.number,
