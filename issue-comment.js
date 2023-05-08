3efef9d486d66d3d864fb5df9629ea89fb714c61
alialaa <hello@alialaa.com>
2023-05-08 14:12:15 +0300

    ewfewf

--- issue-comment.js
+++ issue-comment.js
@@ -7,7 +7,7 @@ module.exports = async ({ github, context }) => {
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: joke.joke,
+    body: joke,
   });
   return joke;
 };
