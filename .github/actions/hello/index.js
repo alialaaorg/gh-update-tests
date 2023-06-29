b2567e84f4a495ae9d3be7037bdd4945dcb02f26
alialaa <hello@alialaa.com>
2023-06-29 16:20:42 +0300

    simple js action 1

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,25 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+try {
+  // throw( new Error("some error message"));
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+  core.setSecret(name);
+  console.log(`Hello ${name}`);
+
+  const name = core.getInput("who-to-greet");
+  console.log(`Hello ${name}`);
+
+  const time = new Date();
+  core.setOutput("time", time.toTimeString());
+
+  core.exportVariable("HELLO_TIME", time);
+
+  core.startGroup("Logging github object");
+  core.debug(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+} catch (error) {
+  core.setFailed(error.message);
+}
