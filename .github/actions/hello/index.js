a5585fc893922b40f63e028c97db4028f610aed5
alialaa <hello@alialaa.com>
2023-06-29 16:35:57 +0300

    fix

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -6,7 +6,6 @@ try {
   core.debug("Debug message");
   core.warning("Warning message");
   core.error("Error message");
-  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const name = core.getInput("who-to-greet");
