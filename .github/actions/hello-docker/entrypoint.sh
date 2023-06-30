8323aff725dbebd24dc60816d1d50a3595442a97
alialaa <hello@alialaa.com>
2023-06-30 12:23:31 +0300

    final

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "::debug ::Running entrypoint.sh"
 echo "Hello $1"
 time=$(date)
-echo $INPUT_WHO_TO_GREET
-echo $HELLO
+echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
+echo "HELLO: $HELLO"
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$time" >> $GITHUB_ENV
