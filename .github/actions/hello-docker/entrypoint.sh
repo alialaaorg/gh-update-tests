29afef07db75239142c5fd34fad960f85cfbbaf7
alialaa <hello@alialaa.com>
2023-06-30 12:19:04 +0300

    #!/bin/sh

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -2,8 +2,7 @@
 echo "::debug ::Running entrypoint.sh"
 echo "Hello $1"
 time=$(date)
-echo $INPUT_WHO-TO-GREET
-echo ${INPUT_WHO-TO-GREET}
+echo $INPUT_WHO_TO_GREET
 echo $HELLO
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$time" >> $GITHUB_ENV
