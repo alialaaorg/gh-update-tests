a4f7ecd658ac22000e3f6d68c7210a30ded84a97
alialaa <hello@alialaa.com>
2023-06-30 14:58:43 +0300

    exit 1

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -6,3 +6,7 @@ echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
 echo "HELLO: $HELLO"
 echo "time=$time" >> $GITHUB_OUTPUT
 echo "HELLO_TIME=$time" >> $GITHUB_ENV
+
+# if <condition> ; then
+#   exit 1
+# fi
